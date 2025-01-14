97ee7310971f05a803b875fb4745ff818320d8ec
ArtakSukiasyan22 <artak.sukiasyan02@gmail.com>
2025-01-14 21:26:52 +0400

    Changed TextEdit part added CodeEditor class added LineNumberArea class deleted pygments_highlighter.py

--- diff_editor.py
+++ diff_editor.py
@@ -5,11 +5,17 @@ from PySide6.QtWidgets import (
     QVBoxLayout,
     QWidget,
     QLabel,
-    
 )
-from PySide6.QtGui import Qt, QFont, QSyntaxHighlighter, QTextCharFormat, QColor, QPainter
-from pygments_highlighter import PygmentsHighlighter
+from PySide6.QtGui import (
+    QFont,
+    QSyntaxHighlighter,
+    QTextCharFormat,
+    QColor,
+    QTextFormat,
+    QIcon,
+)
 from difflib import unified_diff
+from code_editor import CodeEditor
 
 
 class DiffHighlighter(QSyntaxHighlighter):
@@ -20,6 +26,8 @@ class DiffHighlighter(QSyntaxHighlighter):
 
         self.added_format.setBackground(QColor("lightgreen"))
         self.removed_format.setBackground(QColor("lightcoral"))
+        self.added_format.setProperty(QTextFormat.FullWidthSelection, True)
+        self.removed_format.setProperty(QTextFormat.FullWidthSelection, True)
 
     def highlightBlock(self, text):
         if text.startswith("+") and not text.startswith("+++"):
@@ -27,30 +35,12 @@ class DiffHighlighter(QSyntaxHighlighter):
         elif text.startswith("-") and not text.startswith("---"):
             self.setFormat(0, len(text), self.removed_format)
 
-class LineNumberArea(QWidget):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.parent = parent
-        self.setFixedWidth(40)
-    
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setFont(self.parent.old_text.font())
-        painter.setPen(QColor(0, 0, 0))
-        block = self.parent.old_text.document().firstBlock()
-        block_number = 1
-        while block.isValid():
-            painter.drawText(0, block.layout().position().y(), str(block_number))
-            block = block.next()
-            block_number += 1
-
-        return super().paintEvent(event)
-
 
 class DiffEditor(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Diff Editor")
+        self.setWindowIcon(QIcon("logo.png"))
         self.resize(1200, 800)
 
         # Main widget
@@ -66,10 +56,12 @@ class DiffEditor(QMainWindow):
         diff_label = QLabel("Diff Output:")
 
         # Text editors and configure font for editors
-        self.old_text = QPlainTextEdit()
-        self.new_text = QPlainTextEdit()
+        # self.old_text = QPlainTextEdit()
+        # self.new_text = QPlainTextEdit()
+        self.old_text = CodeEditor()
+        self.new_text = CodeEditor()
         self.diff_output = QPlainTextEdit()
-        editor_font = QFont("Courier", 9)
+        editor_font = QFont("Monaco", 10)
         self.old_text.setFont(editor_font)
         self.new_text.setFont(editor_font)
         self.diff_output.setFont(editor_font)
@@ -95,10 +87,6 @@ class DiffEditor(QMainWindow):
         # Connect signals to update diff
         self.old_text.textChanged.connect(self.update_diff)
         self.new_text.textChanged.connect(self.update_diff)
-
-        # # Apply syntax highlighting
-        # self.old_highlighter = PygmentsHighlighter(self.old_text.document())
-        # self.new_highlighter = PygmentsHighlighter(self.new_text.document())
 
         # Apply diff highlighter
         self.diff_highlighter = DiffHighlighter(self.diff_output.document())
